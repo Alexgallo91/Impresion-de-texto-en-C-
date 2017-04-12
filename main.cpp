4c118896e11a8da04f2de3e8cc63b9d412f8aa39
Abraham Araujo Moreno <alex_91y@hotmail.com>
2017-04-11 20:47:02 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "amigo amo el tocino!"
+    <<endl;
+    cout <<"";
+    cout << " y el jamon";
+
+    //o podemos hacer saltos de linea con \n
+    cout<<"hola soy un programa\nmirame\nsoy";
+    cout<<" jodidamente\nfabuloso!!!";
+
+
+    //regresa 0
+    return 0;
+}
